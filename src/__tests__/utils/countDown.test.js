2e04c0eead4c9e0be4433a0d6b197bf08e5d57a7
serg310583 <111340574+serg310583@users.noreply.github.com>
2022-12-03 19:17:13 +0700

    Add files via upload

--- src/__tests__/utils/countDown.test.js
+++ src/__tests__/utils/countDown.test.js
@@ -4,26 +4,17 @@ describe("tests for countDown function", () => {
   it("should countdown from the digit", () => {
      const result = countDown (5);
     expect(result).toBe(5,4,3,2,1);
-  })  
-});
-
-describe("tests for countDown function", () => {
+  }),  
   it("should countdown from the digit", () => {
      const result = countDown ('5');
     expect(result).toBe('5',4,3,2,1);
-  })  
-});
-
-describe("tests for countDown function", () => {
+  }),
   it("should countdown from the digit", () => {
      const result = countDown (-3);
     expect(result).toBe(undefined);
-  })  
-});
-
-describe("tests for countDown function", () => {
+  }),
   it("should countdown from the digit", () => {
      const result = countDown ('two');
     expect(result).toBe(undefined);
-  })  
-});
+  })
+})  
--- src/__tests__/utils/createMonth.test.js
+++ src/__tests__/utils/createMonth.test.js
@@ -1,22 +1,16 @@
-import {createMonth} from  '../../utils/createMonth.js';
-
-describe("test Month by number", () => {
-  it("should be name of month", () => {
-    const result = createMonth(5)
-    expect(result).toBe("Май");
-  })
-})
+import { createMonth } from "../../utils/createMonth.js";
 
 describe("test Month by number", () => {
     it("should be name of month", () => {
-      const result = createMonth(23)
-      expect(result).toBe("Неизвестно");
-    })
-  })
-
-  describe("test Month by number", () => {
+        const result = createMonth(5);
+        expect(result).toBe("Май");
+    }),
     it("should be name of month", () => {
-      const result = createMonth('23')
-      expect(result).toBe("Неизвестно");
+        const result = createMonth(23);
+        expect(result).toBe("Неизвестно");
+    }),
+    it("should be name of month", () => {
+        const result = createMonth("23");
+        expect(result).toBe("Неизвестно");
     })
-  })
+    })
